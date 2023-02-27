76d2ee7512ef4bf31a055416b69f14bf1cf5cbc9
webitproff <webitproff@gmail.com>
2023-02-27 22:32:00 +0200

    cleaning

--- plugins/adminkacota/adminkacota.resources.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$R['Link_Forum_Cotonti'] = 'https://www.cotonti.com/forums/';
-
-$R['Link_Info_Extrafields'] = 'https://www.cotonti.com/docs/ru/admin/Extra_fields-chto_eto';
-$R['Link_Info_Structure'] = 'https://www.cotonti.com/docs/ru/admin/administrators_guide_ru#ch2';

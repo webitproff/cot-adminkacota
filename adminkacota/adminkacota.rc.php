179d0759f0d4ae2f640714518433e32d6a483efb
webitproff <webitproff@gmail.com>
2023-03-02 22:30:04 +0200

    Notes about UIkit 3.16.3 | https://www.getuikit.com | (c) 2014 - 2023 YOOtheme

--- adminkacota/adminkacota.rc.php
+++ adminkacota/adminkacota.rc.php
@@ -11,6 +11,9 @@
 defined('COT_CODE') or die('Wrong URL.');
 
 Resources::addFile('themes/admin/adminkacota/_allsrc/_uikit3/css/uikit.min.css', 'css', 200);
+// https://getuikit.com/docs/introduction 
+// модульный front-end framework для разработки быстрых и мощных веб-интерфейсов.
+// не отключать!
 Resources::addFile('themes/admin/adminkacota/_allsrc/_bs5/css/bootstrap.min.css', 'css', 200);
 
 
@@ -20,6 +23,9 @@ Resources::addFile('themes/admin/adminkacota/_allsrc/_libs/overlayScrollbars/css
 Resources::addFile('themes/admin/adminkacota/css/adminkacota.css', 'css', 200);
 
 Resources::linkFileFooter(cot::$cfg['themes_dir'].'/admin/adminkacota/_allsrc/_uikit3/js/uikit.min.js', 'js', 300);
+// https://getuikit.com/docs/introduction 
+// модульный front-end framework для разработки быстрых и мощных веб-интерфейсов.
+// не отключать!
 Resources::linkFileFooter(cot::$cfg['themes_dir'].'/admin/adminkacota/_allsrc/_bs5/js/bootstrap.bundle.min.js', 'js', 300);
 Resources::linkFileFooter(cot::$cfg['themes_dir'].'/admin/adminkacota/_allsrc/_libs/overlayScrollbars/js/OverlayScrollbars.min.js', 'js', 300);
 Resources::linkFileFooter(cot::$cfg['themes_dir'].'/admin/adminkacota/_allsrc/dist/js/adminlte.min.js', 'js', 300);
